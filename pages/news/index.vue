ba9321cbc6cfe235296ca5573386b7be5c875a9d
HisashiHasebe <hhasebe@diverta.co.jp>
2022-06-29 13:37:57 +0800

    update:newsページ追加

---
+++ pages/news/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="n in response.list" :key="n.slug">
+      <nuxt-link :to="`/news/${n.topics_id}`">{{n.ymd}} {{n.subject}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    async asyncData ({ $axios }) {
+        try {
+            const response = await $axios.$get('/rcms-api/4/news');
+            return { response }
+        }catch (e) {
+            console.log(e.message)
+        }
+    }
+}
+</script>
