310a3a27f34167687d92477ae97e9db871463c39
YuseiAbe <yabe@diverta.co.jp>
2022-07-19 10:31:04 +0900

    refactor(news): put news components together

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -7,16 +7,31 @@
       >
     </div>
 
-    <Pagenator v-if="response.pageInfo" v-bind="{ ...response.pageInfo }" />
+    <Pagenator
+      v-if="
+        $route.query.all === undefined &&
+        $route.query.preview_token === undefined
+      "
+      v-bind="{ ...response.pageInfo }"
+    />
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $axios, route }) {
+    const previewToken = route.query.preview_token;
+    if (previewToken !== undefined) {
+      const previewResponse = await $axios.$get(
+        '/rcms-api/6/news/preview' + '?preview_token=' + previewToken
+      );
+      return { response: { list: [previewResponse.details] } };
+    }
+
     return {
       response: await $axios.$get('/rcms-api/7/news', {
         params: {
+          cnt: route.query.all !== undefined ? 999 : null,
           pageID: route.query.page || 1,
         },
       }),
