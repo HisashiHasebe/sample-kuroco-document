1a273f4418185d655f0e0d216a187ece75ad3257
YuseiAbe <yabe@diverta.co.jp>
2022-07-19 10:16:08 +0900

    refactor: manage components
    - rm try/catch at asyncData()
    - rename/move some components (news etc)
    - split codes into Pagenator

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
+    <p>ニュース一覧ページ{{ this.$route.params.page }}</p>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="`/news/${n.topics_id}`">{{n.ymd}} {{n.subject}}</nuxt-link>
+      <nuxt-link :to="`/news/${n.topics_id}`"
+        >{{ n.ymd }} {{ n.subject }}</nuxt-link
+      >
     </div>
+
+    <Pagenator v-if="response.pageInfo" v-bind="{ ...response.pageInfo }" />
   </div>
 </template>
 
 <script>
 export default {
-    async asyncData ({ $axios }) {
-        try {
-            const response = await $axios.$get('/rcms-api/4/news');
-            return { response }
-        }catch (e) {
-            console.log(e.message)
-        }
-    }
-}
+  async asyncData({ $axios, route }) {
+    return {
+      response: await $axios.$get('/rcms-api/7/news', {
+        params: {
+          pageID: route.query.page || 1,
+        },
+      }),
+    };
+  },
+};
 </script>
--- pages/news_preview/index.vue
+++ pages/news_preview/index.vue
@@ -1,37 +1,35 @@
 <template>
   <div>
     <div v-for="n in response.list" :key="n.slug">
-      <nuxt-link :to="`/news/${n.topics_id}`">{{n.ymd}} {{n.subject}}</nuxt-link>
+      <nuxt-link :to="`/news/${n.topics_id}`"
+        >{{ n.ymd }} {{ n.subject }}</nuxt-link
+      >
     </div>
   </div>
 </template>
 
-
 <script>
 export default {
-    async asyncData ({ route, $axios }) {
+  async asyncData({ route, $axios }) {
+    const requestNews = async () => {
+      const response = await $axios.$get('/rcms-api/6/news');
+      return { response };
+    };
+    const requestNewsPreview = async (previewToken) => {
+      const response = await $axios.$get(
+        '/rcms-api/6/news/preview' + '?preview_token=' + previewToken
+      );
+      return { response: { list: [response.details] } };
+    };
 
-        const requestNews = async () => {
-            const response = await $axios.$get('/rcms-api/6/news');
-            return { response };
-        };
-        const requestNewsPreview = async (previewToken) => {
-            const response = await $axios.$get('/rcms-api/6/news/preview' + '?preview_token=' + previewToken);
-            return { response: { list: [response.details] } };
-        };
-
-        // URLのクエリに、
-        // preview_tokenが存在する場合は、previewエンドポイントへ、
-        // preview_tokenが存在しない場合は、newsエンドポイントへ、
-        // リクエストします。
-        try {
-            const previewToken = route.query.preview_token;
-            return previewToken !== undefined ?
-                await requestNewsPreview(previewToken) :
-                await requestNews();
-        }catch (e) {
-            console.log(e.message)
-        }
-    }
-}
+    // URLのクエリに、
+    // preview_tokenが存在する場合は、previewエンドポイントへ、
+    // preview_tokenが存在しない場合は、newsエンドポイントへ、
+    // リクエストします。
+    const previewToken = route.query.preview_token;
+    return previewToken !== undefined
+      ? await requestNewsPreview(previewToken)
+      : await requestNews();
+  },
+};
 </script>
