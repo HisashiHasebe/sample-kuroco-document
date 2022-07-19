30cf674fbeb3b67a630e8d6bb740414dbfef2391
YuseiAbe <yabe@diverta.co.jp>
2022-07-19 12:05:44 +0900

    refactor(subscribe): chores

--- pages/subscribe/index.vue
+++ pages/subscribe/index.vue
@@ -1,69 +1,71 @@
 <template>
   <div>
     <form>
-    <h1>マガジン登録</h1>
+      <h1>マガジン登録</h1>
       <p v-if="resultMessage !== null">
         {{ resultMessage }}
       </p>
-    <input v-model="emailEntered" name="email" type="email" placeholder="email">
-    <button v-on:click.prevent="subscribeSubmit">購読する</button>    
-    <button v-on:click.prevent="unsubscribeSubmit">購読解除する</button>
-    <button v-on:click.prevent="sendTestMail">テスト送信する</button>
-   </form>
+      <input
+        v-model="emailEntered"
+        name="email"
+        type="email"
+        placeholder="email"
+      />
+      <button @click.prevent="subscribeSubmit">購読する</button>
+      <button @click.prevent="unsubscribeSubmit">購読解除する</button>
+      <button @click.prevent="sendTestMail">テスト送信する</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       emailEntered: '',
       resultMessage: null,
-      }
+    };
   },
   methods: {
     //購読するクリック時の動作
     async subscribeSubmit() {
       try {
-        const payload = {
-                    email: this.emailEntered
-                }
-        // post data
         const response = await this.$axios.$post(
-          `/rcms-api/11/magazine_subscribe/1`, payload)
-          this.resultMessage = response.messages[0]
+          `/rcms-api/11/magazine_subscribe/1`,
+          {
+            email: this.emailEntered,
+          }
+        );
+        this.resultMessage = response.messages[0];
       } catch (error) {
-         this.resultMessage = error.response.data.errors[0].message
+        this.resultMessage = error.response.data.errors[0].message;
       }
     },
     //購読解除するクリック時の動作
     async unsubscribeSubmit() {
       try {
-        const payload = {
-                    email: this.emailEntered
-                }
-        // post data
         const response = await this.$axios.$post(
-          `/rcms-api/11/magazine_unsubscribe/1`, payload)
-          this.resultMessage = response.messages[0]
+          `/rcms-api/11/magazine_unsubscribe/1`,
+          {
+            email: this.emailEntered,
+          }
+        );
+        this.resultMessage = response.messages[0];
       } catch (error) {
-         this.resultMessage = error.response.data.errors[0].message
+        this.resultMessage = error.response.data.errors[0].message;
       }
     },
     //テスト送信するクリック時の動作
     async sendTestMail() {
       try {
-        const payload = {
-                    mail_to: this.emailEntered
-                }
-        // post data
-        const response = await this.$axios.$post(
-          `/rcms-api/11/magazine_sendMail`, payload)
-          this.resultMessage = 'テストメールを送信しました。'
+        await this.$axios.$post(`/rcms-api/11/magazine_sendMail`, {
+          mail_to: this.emailEntered,
+        });
+        this.resultMessage = 'テストメールを送信しました。';
       } catch (error) {
-         this.resultMessage = error.response.data.errors[0].message
+        this.resultMessage = error.response.data.errors[0].message;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
