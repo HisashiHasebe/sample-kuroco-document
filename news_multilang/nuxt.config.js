68b11b7f91819d2befe29dcd1b09479153d0ecbe
YuseiAbe <yabe@diverta.co.jp>
2022-07-20 10:38:56 +0900

    chore: manage to apply subpackages

---
+++ news_multilang/nuxt.config.js
@@ -0,0 +1,75 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'sample-kuroco-document',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/svg+xml', href: '/icon_kuroco.svg' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // 多言語設定利用のため追加 https://kuroco.app/ja/docs/tutorials/building-a-multi-language-site/
+    [
+      'nuxt-i18n',
+      {
+        strategy: 'prefix_and_default',
+        // 切り替える言語を定義
+        locales: [
+          { code: 'ja', file: 'ja.json' },
+          { code: 'en', file: 'en.json' },
+        ],
+        // デフォルトの言語を設定
+        defaultLocale: 'ja',
+        vueI18nLoader: true,
+        lazy: true,
+        // jsonファイルを保存したディレクトリを指定
+        langDir: 'locales/',
+      },
+    ],
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: process.env.BASE_URL,
+    // クロスオリジンでのcookieを有効化 https://kuroco.app/ja/docs/tutorials/integrate-login/
+    credentials: true,
+    withCredentials: true
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ search_keyword/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <main>
+      <div>
+        <img src="/logo_Kuroco_black.svg" />
+      </div>
+      <div>
+        Visit our <a href="https://kuroco.app/">website</a> if you are new to
+        Kuroco!
+      </div>
+    </main>
+    <footer>
+      Copyright © {{ new Date().getFullYear() }} Diverta Inc. All rights
+      reserved.
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.container {
+  display: grid;
+  grid-template:
+    'main' calc(100vh - 3rem)
+    'footer' 3rem /
+    auto;
+}
+
+main {
+  grid-area: main;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+}
+footer {
+  grid-area: footer;
+  color: #eeedfa;
+  background: #000000;
+  border-top: 1px solid #3f4044;
+  padding: 10px 20px;
+  font-size: 0.7rem;
+}
+</style>
