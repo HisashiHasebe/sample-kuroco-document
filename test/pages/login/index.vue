6071e610ac8064dba38f7ad2bb0ae4c1e6d309eb
HisashiHasebe <hhasebe@diverta.co.jp>
2022-08-19 13:36:22 +0800

    ログインの最初のページ追加

---
+++ test/pages/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="login">
+    <input v-model="email" name="email" type="email" placeholder="email" />
+    <input
+      v-model="password"
+      name="password"
+      type="password"
+      placeholder="password"
+    />
+    <button type="submit">ログイン</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login () {
+        console.log(this.email, this.password)
+    }
+  },
+};
+</script>
